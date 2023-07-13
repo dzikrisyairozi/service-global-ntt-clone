bb7104015d60443f3c3b8edcef517c90577587fd
dikiogres <dikiogres@gmail.com>
2023-07-14 00:57:05 +0700

    feat: navbar mock

--- src/app/app-routing.module.ts
+++ src/app/app-routing.module.ts
@@ -1,8 +1,13 @@
 import { NgModule } from '@angular/core';
 import { RouterModule, Routes } from '@angular/router';
 import { SandboxComponent } from './sandbox/sandbox.component';
+import { AppComponent } from './app.component';
 
 const routes: Routes = [
+  {
+    path: '/',
+    component: AppComponent,
+  },
   {
     path: 'sandbox',
     component: SandboxComponent,
