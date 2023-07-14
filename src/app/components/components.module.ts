65377461660a5f30a235dce638c06bc79926176c
dikiogres <dikiogres@gmail.com>
2023-07-14 19:41:14 +0700

    feat: arrow link component

--- src/app/components/components.module.ts
+++ src/app/components/components.module.ts
@@ -7,6 +7,8 @@ import { TypographyComponent } from "./typography/typography.component";
 import { TopFooterComponent } from "./layout/footer/top-footer/top-footer.component";
 import { BottomFooterComponent } from "./layout/footer/bottom-footer/bottom-footer.component";
 import { MaterialDesignModule } from "../lib/material.design.module";
+import { ArrowLinkComponent } from './arrow-link/arrow-link.component';
+import { SpeakExpertComponent } from "../pages/home/speak-expert/speak-expert.component";
 
 @NgModule({
   declarations: [    
@@ -16,6 +18,7 @@ import { MaterialDesignModule } from "../lib/material.design.module";
     TypographyComponent,
     TopFooterComponent,
     BottomFooterComponent,
+    ArrowLinkComponent,
 ],
   imports: [
     CommonModule,
@@ -28,6 +31,7 @@ import { MaterialDesignModule } from "../lib/material.design.module";
     TypographyComponent,
     TopFooterComponent,
     BottomFooterComponent,
+    ArrowLinkComponent,
   ],
 })
 export class ComponentsModule {}
