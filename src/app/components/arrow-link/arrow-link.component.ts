9fceb2f384d20d3c718ed6ec714af6b4ab658328
dikiogres <dikiogres@gmail.com>
2023-07-14 19:41:14 +0700

    feat: arrow link component

---
+++ src/app/components/arrow-link/arrow-link.component.ts
@@ -0,0 +1,29 @@
+import { Component, Input } from '@angular/core';
+
+@Component({
+  selector: 'app-arrow-link',
+  template: `
+    <button
+      [ngClass]="{
+        ' bg-white text-primary transition duration-200 ease-in-out hover:bg-gray-200':
+          variant === 'accent',
+        ' bg-primary text-white transition duration-200 ease-in-out hover:bg-accent':
+          variant === 'primary'
+      }"
+
+      class="mt-[40px] rounded-md px-4 py-2 text-sm flex items-center group"
+    >
+      <p>
+        <ng-content></ng-content>
+      </p>
+      <mat-icon
+        class="ml-4 transition duration-500 group-hover:ml-8 animate-ease-in-out"
+        >arrow_forward</mat-icon
+      >
+    </button>
+  `,
+  styleUrls: ['./arrow-link.component.css'],
+})
+export class ArrowLinkComponent {
+  @Input() variant: 'primary' | 'accent' = 'primary';
+}
