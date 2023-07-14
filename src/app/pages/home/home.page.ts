30d8e8b97c8fc1fa3f495d8362d3dc61792e1461
dikiogres <dikiogres@gmail.com>
2023-07-14 14:44:31 +0700

    feat: add typography component

--- src/app/pages/home/home.page.ts
+++ src/app/pages/home/home.page.ts
@@ -1,7 +1,7 @@
 import { Component } from '@angular/core';
 
 @Component({
-  selector: 'app-home',
+  selector: 'page-home',
   templateUrl: './home.page.html',
   styleUrls: ['./home.page.css']
 })
--- src/app/pages/sandbox/sandbox.page.ts
+++ src/app/pages/sandbox/sandbox.page.ts
@@ -1,7 +1,7 @@
 import { Component } from '@angular/core';
 
 @Component({
-  selector: 'app-sandbox',
+  selector: 'page-sandbox',
   templateUrl: './sandbox.page.html',
   styleUrls: ['./sandbox.page.css']
 })
