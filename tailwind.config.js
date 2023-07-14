31c692f4dbbdf12c81a500af6375b5beccb605f8
dikiogres <dikiogres@gmail.com>
2023-07-14 21:19:14 +0700

    styles: custom ping animated

--- tailwind.config.js
+++ tailwind.config.js
@@ -1,16 +1,32 @@
-/** @type {import('tailwindcss').Config} */
+const plugin = require('tailwindcss/plugin');
+
 module.exports = {
-  content: [
-    "./src/**/*.{html,ts}",
-  ],
+  content: ['./src/**/*.{html,ts}'],
   theme: {
-    extend: {    
+    extend: {
+      animation: {
+        'custom-ping': 'custom-ping 2s infinite',
+      },
+      keyframes: {
+        'custom-ping': {
+          '75%, 100%': { transform: 'scale(1.5)', opacity: '0' },
+        },
+      },
       colors: {
         primary: '#0072BC',
         accent: '#0FC8F2',
       },
     },
   },
-  plugins: [],
-}
+  plugins: [
+    plugin(function ({ addUtilities }) {
+      const newUtilities = {
+        '.animate-custom-ping': {
+          animation: 'custom-ping 2s infinite',
+        },
+      };
 
+      addUtilities(newUtilities, ['responsive', 'hover']);
+    }),
+  ],
+};
