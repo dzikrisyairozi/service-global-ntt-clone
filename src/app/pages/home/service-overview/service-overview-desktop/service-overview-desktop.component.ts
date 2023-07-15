6c837ae988983cdd7ae753729eda4e22639ee51b
dikiogres <dikiogres@gmail.com>
2023-07-15 18:50:49 +0700

    feat: mobile responsiveness

--- src/app/pages/home/service-overview/service-overview-desktop/service-overview-desktop.component.ts
+++ src/app/pages/home/service-overview/service-overview-desktop/service-overview-desktop.component.ts
@@ -3,7 +3,6 @@ import { Component } from '@angular/core';
 @Component({
   selector: 'app-service-overview-desktop',
   templateUrl: './service-overview-desktop.component.html',
-  styleUrls: ['./service-overview-desktop.component.css']
 })
 export class ServiceOverviewDesktopComponent {
 
--- src/app/pages/home/service-overview/service-overview-mobile/service-overview-mobile.component.ts
+++ src/app/pages/home/service-overview/service-overview-mobile/service-overview-mobile.component.ts
@@ -3,7 +3,6 @@ import { Component } from '@angular/core';
 @Component({
   selector: 'app-service-overview-mobile',
   templateUrl: './service-overview-mobile.component.html',
-  styleUrls: ['./service-overview-mobile.component.css']
 })
 export class ServiceOverviewMobileComponent {
 
