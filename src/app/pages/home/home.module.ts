d0d0f5195bd773acc1ebeab4cde2f3f2000c28d7
dikiogres <dikiogres@gmail.com>
2023-07-15 14:07:48 +0700

    fix: web element module conflict

--- src/app/pages/home/home.module.ts
+++ src/app/pages/home/home.module.ts
@@ -1,4 +1,4 @@
-import { NgModule } from '@angular/core';
+import { CUSTOM_ELEMENTS_SCHEMA, NgModule } from '@angular/core';
 import { CommonModule } from '@angular/common';
 import { HomePage } from './home.page';
 import { SpeakExpertComponent } from './speak-expert/speak-expert.component';
@@ -6,7 +6,12 @@ import { WhyNttComponent } from './why-ntt/why-ntt.component';
 import { RelatedInsightsComponent } from './related-insights/related-insights.component';
 import { FeaturedBannerComponent } from './featured-banner/featured-banner.component';
 import { ServiceOverviewComponent } from './service-overview/service-overview.component';
+import { ComponentsModule } from 'src/app/components/components.module';
+import {register} from 'swiper/element/bundle';
+import { MaterialDesignModule } from 'src/app/lib/material.design.module';
+import { SwiperDirective } from 'src/app/directives/swiper.directive';
 
+register();
 
 @NgModule({
   declarations: [
@@ -19,6 +24,18 @@ import { ServiceOverviewComponent } from './service-overview/service-overview.co
   ],
   imports: [
     CommonModule,
+    MaterialDesignModule,
+    ComponentsModule,
+    SwiperDirective,
   ],
+  exports: [
+    HomePage,
+    SpeakExpertComponent,
+    WhyNttComponent,
+    RelatedInsightsComponent,
+    FeaturedBannerComponent,
+    ServiceOverviewComponent,
+  ],
+  schemas: [CUSTOM_ELEMENTS_SCHEMA],
 })
 export class HomeModule { }
